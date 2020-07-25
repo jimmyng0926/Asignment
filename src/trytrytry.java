31e5b930c0818f4a65935bd541c709b22fac31f4
tfy513 <68457641+tfy513@users.noreply.github.com>
2020-07-26 00:05:22 +0800

    can see mah, from fy

--- src/trytrytry.java
+++ src/trytrytry.java
@@ -1,9 +1,6 @@
 
 public class trytrytry {
 	public void main(String [] args) {
-		System.out.print("jimmy ng change jorr u all can see at 12.003?");
-			System.out.print("jimmy ng change jorr u all can see at 12.003?");
-			System.out.print("jimmy ng change jorr u all can see at 12.003?");
-			System.out.print("jimmy ng change jorr u all can see at 12.003?");	System.out.print("jimmy ng change jorr u all can see at 12.003?");
+		System.out.print("can see mah?");
 	}
 }

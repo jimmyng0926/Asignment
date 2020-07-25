8adc3f4a866a79384734d2e18e333768cd6f5649
jimmyng0926 <68769919+jimmyng0926@users.noreply.github.com>
2020-07-26 00:04:56 +0800

    Update trytrytry.java

--- src/trytrytry.java
+++ src/trytrytry.java
@@ -2,5 +2,8 @@
 public class trytrytry {
 	public void main(String [] args) {
 		System.out.print("jimmy ng change jorr u all can see at 12.003?");
+			System.out.print("jimmy ng change jorr u all can see at 12.003?");
+			System.out.print("jimmy ng change jorr u all can see at 12.003?");
+			System.out.print("jimmy ng change jorr u all can see at 12.003?");	System.out.print("jimmy ng change jorr u all can see at 12.003?");
 	}
 }
